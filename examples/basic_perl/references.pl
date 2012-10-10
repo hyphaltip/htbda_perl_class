7228e4acf48b5da0fee60b69257be93ea2b3bb38
Jason Stajich <jason.stajich@ucr.edu>
2012-10-10 13:50:22 -0700

    more reference examples

--- examples/basic_perl/references.pl
+++ examples/basic_perl/references.pl
@@ -1,7 +1,43 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
+
+
+my $str = "AAGAG";
+
+my $str_ref = \$str;
+
+print "String: $str Ref: $str_ref\n";
+print "De-ref Ref:",$$str_ref,"\n";
+
+
+my @array = (20,50,60);
+
+my $array_ref = \@array;
+
+print "Array: @array Ref: $array_ref\n";
+print "Deref Ref: ",join(",",@$array_ref),"\n";
+
+my %ha = ('apple' => 'green', 'bannana' => 'yellow');
+
+my $hash_ref = \%ha;
+
+print "Hash: ",join(",",keys %ha)," Ref: $hash_ref\n";
+print "Deref Ref: ",join(",",keys %$hash_ref),"\n";
+
+$hash_ref->{orange} = 'orange';
+print "Hash: ",join(",",keys %ha)," Ref: $hash_ref\n";
+print "Deref Ref: ",join(",",keys %$hash_ref),"\n";
+
+print "\n";
+my $hash_ref2 = {%ha};
+$hash_ref2->{grape} = 'purple';
+print "Hash: ",join(",",keys %ha)," Ref: $hash_ref\n";
+print "Deref Ref: ",join(",",keys %$hash_ref),"\n";
+print "Deref Ref2: ",join(",",keys %$hash_ref2),"\n";
+
+print "\n==\n";
+
 my $url = 'http://courses.stajich.org/public/gen220/data/Ncrassa_OR74A_InterproDomains.tab';
 open(my $fh => "curl $url |") || die $!;
 my %genes;
