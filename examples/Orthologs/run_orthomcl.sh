a814cae301cdae28d7d131aacecb3a751a8e8075
Jason Stajich <jason.stajich@ucr.edu>
2012-11-07 13:23:40 -0800

    updated for some more controls

--- examples/Orthologs/run_orthomcl.sh
+++ examples/Orthologs/run_orthomcl.sh
@@ -3,16 +3,25 @@
 module load orthoMCL
 module load ncbi-blast
 
-#mysqladmin drop -u orthomcl -p0rthMC1 orthomcl_testMYNAME
-#mysqladmin create -u orthomcl -p0rthMC1 orthomcl_testMYNAME
-#orthomclInstallSchema orthomcl.config
+# copy the config file from the shared location
+if [ ! -f orthomcl.config ]; then
+ cp /srv/projects/db/GEN220/orthomcl/orthomcl.config .
+done
+# Grab the password from the file
+PASS=`grep dbPassword orthomcl.config | awk -F= '{print $2}'`
+# drop db to get a clean slate
+mysqladmin drop -f -u orthomcl -p$PASS orthomcl_testMYNAME
+mysqladmin create -u orthomcl -p$PASS orthomcl_testMYNAME
+orthomclInstallSchema orthomcl.config
 
-#mkdir proteomes
-#cd proteomes
-#../download.sh
-#cd ..
+if [ ! -d proteomes ]; then
+ mkdir proteomes
+ cd proteomes
+ ../download.sh
+ cd ..
+fi
 
-mkdir cleanseq
+mkdir -p cleanseq
 cd cleanseq
 
 for file in ../proteomes/*.pep
@@ -22,20 +31,33 @@ do
  orthomclAdjustFasta $base $file 4 
 done
 cd ..
-orthomclFilterFasta cleanseq 10 10
 
+
+orthomclFilterFasta cleanseq 10 10
 makeblastdb -in goodProteins.fasta -title "OrthoMCLPeps" -dbtype prot
 
+# This will run BLAST - may take an hour or two
 if [ ! -f goodProteins.BLASTP.tab ]; then
  blastp -query goodProteins.fasta -db goodProteins.fasta -num_threads 4 -outfmt 6 -out goodProteins.BLASTP.tab -evalue 1e-3
 fi
+
+# This will reformat the BLAST data into something to load into the database
 if [ ! -f goodProteins.BLASTP.bpo ]; then
  orthomclBlastParser goodProteins.BLASTP.tab cleanseq > goodProteins.BLASTP.bpo
 fi
 
+# Load the data into the DB
 orthomclLoadBlast orthomcl.config goodProteins.BLASTP.bpo
+
+# now run the ortholog/paralog initial finding
+rm -rf pairs pairs.log 
 orthomclPairs orthomcl.config  pairs.log cleanup=no
+
+# Dump out the ortholog groups and the mclInput file
 orthomclDumpPairsFiles orthomcl.config
 
+# Run mcl for clustering
 mcl mclInput  --abc -I 1.5 -o mclOutput.I15.out
+
+# convert the MCL clusters into OrthoMCL groups
 orthomclMclToGroups OG 1 < mclOutput.I15.out > mclGroups.I15.table
